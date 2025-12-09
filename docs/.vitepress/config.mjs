34c502da89b77bcef7e34c77db230bb700dacd5d
February-bird <84173020@qq.com>
2025-12-09 18:00:01 +0800

    更新至0.2.7正式版

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
     logo: "/logo.svg",
     nav: [
       {
-        text: "0.2.7 beta",
+        text: "0.2.7",
         items: [
           {
             text: "更新日志",
